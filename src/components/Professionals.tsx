87ade909be2d25ff1991cdcc4c26774eb3435a8a
Nico Cafure <nicocafure@gmail.com>
2021-10-19 12:30:09 -0300

    Agregando paginación del profesionales

--- src/components/Professionals.tsx
+++ src/components/Professionals.tsx
@@ -1,5 +1,6 @@
 import { useQuery } from "react-query";
-import { Table, Tag, Space } from 'antd';
+import { Table, Tag, Space , Pagination} from 'antd';
+import { useState ,useEffect} from "react";
 
 interface IProfessional{
 email:string,
@@ -20,8 +21,8 @@ count: number,
 
 
 
-async function fetchProfessionals(){
-  const response = await fetch("http://challenge.radlena.com/api/v1/professionals/")
+async function fetchProfessionals(pageN:number){
+  const response = await fetch(`http://challenge.radlena.com/api/v1/professionals/?page=${pageN}`)
   if(!response.ok){
     throw new Error ("Recuperando lista de profesionales")
   }
@@ -31,17 +32,31 @@ async function fetchProfessionals(){
 
 const Professionals : React.FunctionComponent = () => {
 
-const query = useQuery<IResults,Error>("PROFESSIONALS" , fetchProfessionals)
+  const [page, setPage] = useState(1)
+
+const query = useQuery<IResults,Error>(['PROFESSIONALS', page],()=>fetchProfessionals(page), { keepPreviousData : true })
 if(query.isLoading){
   return <div>Cargando profesionales </div>
+
 }
 if (query.isError){
   return <div>Error cargando profesionales</div>
 }
 
 
-
+ 
 const columns = [
+  {
+    title: 'Profile',
+    key: 'profile_image',
+    dataIndex: 'profile_image',
+    render: (img:string) => (
+      <><div className="data-profile-img">
+       <img src={img}></img>
+       </div>
+      </>
+    ),
+  },
   {
     title: 'Nombre',
     dataIndex: 'first_name',
@@ -57,16 +72,7 @@ const columns = [
     dataIndex: 'id',
     key: 'id',
   },
-  {
-    title: 'Profile',
-    key: 'profile_image',
-    dataIndex: 'profile_image',
-    render: (img:string) => (
-      <>
-       <img src={img}></img>
-      </>
-    ),
-  }
+  
 ];
 
 
@@ -80,12 +86,14 @@ const columns = [
 
     return (<div>
 
-  {query.data?.results.map(professional=><p>{professional.email}</p>)}
 
 
 
-  <Table columns={columns} dataSource={query.data?.results} />,
 
+  <Table columns={columns} dataSource={query.data?.results} pagination={false}  />,
+  <Pagination onChange={(pageone)=>{
+    setPage(pageone)
+  }} total={query.data?.count} />
 
 
 
